b203d96cbcb51187d460bc77f9988bc422470471
javasop <osamah.net.m@gmail.com>
2016-01-16 01:47:07 -0700

    added quintus to deps

---
+++ app/client/main.js
@@ -0,0 +1,3 @@
+var Rx = require('rx');
+require("./src/scss/main.scss");
+require("font-awesome/scss/font-awesome.scss");
--- gulp/server.js
+++ gulp/server.js
@@ -1,12 +1,7 @@
 'use strict';
-
 var gulp = require('gulp');
-
 var serve = require('browser-sync');
-
 var util = require('util');
-
-var nodemon = require('gulp-nodemon');
 
 module.exports = function (options) {
 
@@ -19,20 +14,4 @@ module.exports = function (options) {
             }
         });
     });
-    gulp.task('serve:server', function () {
-        nodemon({
-            script: './app/server/main.js'
-            , ext: 'js html'
-            , env: { 'NODE_ENV': 'development' }
-        })
-    })
-    gulp.task('serve:python', function () {
-
-    })
-    gulp.task('serve:scala', function () {
-
-    })
-
-
-
 };
