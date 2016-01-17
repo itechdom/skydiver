2f4f45621cb05c219372c4688a7db54c32f038dd
javasop <osamah.net.m@gmail.com>
2016-01-17 15:29:17 -0700

    added background

--- app/client/components/game/main.js
+++ app/client/components/game/main.js
@@ -15,6 +15,7 @@ class gameMain{
 }
 
 function init(){
+
 	var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-example', { preload: preload, create: create });
 
 	var sprite;
@@ -25,8 +26,11 @@ function init(){
 		//  There are 18 frames in the PNG - you can leave this value blank if the frames fill up the entire PNG, but in this case there are some
 		//  blank frames at the end, so we tell the loader how many to load
 		game.load.spritesheet('uniqueKey', 'dist/diver-pixel.png', 72, 72,2);
+		game.load.image("background", "assets/sky-background.png");
+
 	}
 	function create() {
+		var background = game.add.tileSprite(0, 0, 1000, 600, "background");
 
 		var sprite = game.add.sprite(100, 200, 'uniqueKey');
 		sprite.animations.add('walk');
